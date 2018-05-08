0d0b4726b7cef5c8a501166255e1da4bacc9c045
Jason E Bailey <jeb@apache.org>
2018-05-08 10:54:52 -0400

    SLING-7657 update package version
    per baseline

--- src/main/java/org/apache/sling/servlethelpers/package-info.java
+++ src/main/java/org/apache/sling/servlethelpers/package-info.java
@@ -19,5 +19,5 @@
 /**
  * Mock implementation of selected Servlet-related Sling APIs.
  */
-@org.osgi.annotation.versioning.Version("1.2")
+@org.osgi.annotation.versioning.Version("1.3")
 package org.apache.sling.servlethelpers;
